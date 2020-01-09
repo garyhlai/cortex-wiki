a9cb08fc19be3534fc1a2a1307463607c5a549de
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2020-01-09 15:25:17 -0500

    adds python script for converting user input

---
+++ user_input/convert.py
@@ -0,0 +1,21 @@
+import sys
+from PIL import Image
+
+img = Image.open(sys.argv[1])
+img = img.resize((32,32))
+img = img.load()
+
+h = '0123456789abcdef'
+s = ''
+for i in range(32):
+    for j in range(32):
+        for k in img[i, j]:
+            s += h[k // 16] + h[k % 16]
+ret = []
+for i in range(0, len(s), 64):
+    if i + 64 <= len(s):
+        ret.append('0x' + s[i:i+64])
+    else:
+        ret.append('0x' + '0' * (len(s) - i + 64) + s[i:])
+
+print(ret)
